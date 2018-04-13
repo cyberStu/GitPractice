5afbbf44643dec8763a90ae2b21889f0b8f315cc
ayz.umarova <ayz.umarova@10.0.0.46>
2018-04-13 12:43:35 -0500

    Added 2 for

--- src/git/GitClass.java
+++ src/git/GitClass.java
@@ -2,15 +2,23 @@ package git;
 
 public class GitClass {
 public static void main(String[] args) {
+	System.out.println("Try from Arzuv");
 	
+	for (int i=0; i<9; i++) {
+		System.out.println("Tiramusi was perfect, Arzuvjon");
+	}
 	
-	System.out.println("Hello Ayzamal!!!");
+	for (int i=0; i<9; i++) {
+		System.out.println("Tiramusi was perfect, Arzuvjon");
+	}
+	
 	System.out.println("Hello from Ayzhamal");
 	
 	
 	String cake= "Tiramisu";
 	System.out.println("It was good to have you as guest, Ayzhamal");
 	System.out.println("New try");
+	System.out.println("Aloha");
 
 	
 	
