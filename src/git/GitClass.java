1ede358f4cd6d4d25d2ad31c0fb4bc7282555718
ayz.umarova <ayz.umarova@10.0.0.46>
2018-04-13 12:34:15 -0500

    Added tirumasu

--- src/git/GitClass.java
+++ src/git/GitClass.java
@@ -2,9 +2,12 @@ package git;
 
 public class GitClass {
 public static void main(String[] args) {
+	System.out.println("Try from Arzuv");
 	
+	for (int i=0; i<9; i++) {
+		System.out.println("Tiramusi was perfect, Arzuvjon");
+	}
 	
-	System.out.println("Hello Ayzamal!!!");
 	System.out.println("Hello from Ayzhamal");
 	
 	
