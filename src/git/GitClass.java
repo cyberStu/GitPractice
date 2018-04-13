5ed38d280e664624573a2e3c6097a1caae513845
arzuvbaymiyeva <arzuvbaymiyeva@10.0.0.158>
2018-04-13 12:29:29 -0500

    New message

--- src/git/GitClass.java
+++ src/git/GitClass.java
@@ -2,10 +2,15 @@ package git;
 
 public class GitClass {
 public static void main(String[] args) {
-	System.out.println("Try from Arzuv");
+	
 	
 	System.out.println("Hello Ayzamal!!!");
 	System.out.println("Hello from Ayzhamal");
+	
+	
+	System.out.println("New try");
+
+	
 	
 }
 }
